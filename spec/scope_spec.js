99bcb0a01764c6ae4008c597628b03e44e693491
Rudrasish <rudrapanda117@gmail.com>
2017-12-12 21:47:39 +0530

    Creating $watch and $digest function

--- spec/scope_spec.js
+++ spec/scope_spec.js
@@ -1,3 +1,6 @@
+/* jshint globalstrict: true */
+/* global Scope: false */
+
 'use strict'
 
 describe("Scope", function(){
@@ -7,5 +10,24 @@ describe("Scope", function(){
         scope.aProperty = 1;
 
         expect(scope.aProperty).toBe(1);
+    });
+
+    describe("digest",function(){
+        var scope;
+
+        beforeEach(function(){
+            scope = new Scope();
+        });
+
+        it("call the listener function of a watch on first $digest",function(){
+            var watchFn = function(){ return 'wat';}
+            var listnerFn = jasmine.createSpy();
+
+            scope.$watch(watchFn,listnerFn);
+
+            scope.$digest();
+
+            expect(listnerFn).toHaveBeenCalled();
+        });
     });
 });
--- src/Scope.js
+++ src/Scope.js
@@ -1,3 +1,22 @@
-'use strict';
+/* jshint globalstrict: true */
+"use strict";
 function Scope() {
+   
+
+    this.$$watchers = [];
 }
+
+Scope.prototype.$watch = function (watchFn, listnerFn) {
+    var watcher = {
+        watchFn:watchFn,
+        listnerFn:listnerFn
+    };
+
+    this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function(){
+    _.forEach(this.$$watchers,function(watcher){
+        watcher.listnerFn();
+    })
+};
